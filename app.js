3086036b4524e3322fa2daf36bba39278f3dee09
kyleines <kyleines@gmail.com>
2021-06-03 10:38:03 -0500

    require express and setup localhost:3000

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+
+
+
+
+// Local server connection
+app.listen(3000, () => {
+    console.log(Date());
+    console.log("App running on localhost:3000");
+});
